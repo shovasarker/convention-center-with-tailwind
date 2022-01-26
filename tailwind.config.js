deec344d805cfbb21a8dfdfc3009c04ba9929021
ShovaSarker <shovasarker01@gmail.com>
2022-01-26 23:58:01 +0600

    Package asection added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#02B290',
+        paste: {
+          200: '#97B0AC',
+          400: '#8AF4E0',
+          500: '#02B290',
+          700: '#1C3E37',
+          800: '#0C2924',
+        },
+      },
+      fontSize: {
+        tiny: '15px',
+      },
+      boxShadow: {
+        '3xl': '0px 42px 34px rgba(63, 135, 245, 0.25)',
+      },
+      spacing: {
+        '0p': '0%',
+        '3p': '3%',
+        '10p': '10%',
+        '13p': '13%',
+      },
+      backgroundPosition: {
+        '115p': '115%',
       },
       borderRadius: {
         circle: '50%',
+      },
+      container: {
+        center: true,
       },
     },
   },
